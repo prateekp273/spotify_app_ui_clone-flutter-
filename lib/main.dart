607a1de84d590972bb95eede2a21a60509d098a5
prateekp273 <prateekp18012000@gmail.com>
2023-08-09 13:42:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/view/song_player.dart';
+
+void main() {
+  // system overlays
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      // home: const SigninScreen(),
+      home: const AudioPlayerScreen(),
+    );
+  }
+}
