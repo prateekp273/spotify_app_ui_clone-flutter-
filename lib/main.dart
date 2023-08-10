2f689eb884709006e67040edf2f46dc12e0d1dfe
prateekp273 <prateekp18012000@gmail.com>
2023-08-10 10:58:25 +0530

    updating

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Spotify',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
